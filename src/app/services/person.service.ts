c485b189c6100e26e8d29ed077bd665284406796
Michael Adaikalaraj <michaeljaison.me@gmail.com>
2019-10-07 16:55:32 -0400

    API link updated

--- src/app/services/person.service.ts
+++ src/app/services/person.service.ts
@@ -10,7 +10,7 @@ import { Person } from '../models/person';
 })
 export class PersonService {
 
-  private endpoint = 'http://localhost:8080/api/v1/users';
+  private endpoint = 'https://technicaltestrestapi.herokuapp.com//api/v1/users';
 
   private httpOptions = {
     headers: new HttpHeaders({
